f80054e2a99d71fd711dd916bef045516262b0ba
AnasLaHBouB <anaslahbou11@gmail.com>
2025-02-28 08:48:30 +0000

    add before merging with ahmed

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import "./index.css";
-import Home from "./pages/Home";
-import Partenaire from "./pages/Partenaire";
-import Morale from "./pages/Morale";
-import Physique from "./pages/Physique";
-import Client from "./pages/Client";
-import Fournisseur from "./pages/Fournisseur";
-import Chauffeur from "./pages/Chauffeur";
-import Camion from "./pages/Camion";
-import Cabine from "./pages/Cabine";
-import Remorque from "./pages/Remorque";
-import Layout from "./components/layout";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      {/* Home Route */}
-      <Route index element={<Home />} />
-      <Route path="home" element={<Home />} />
-
-      {/* Partenaire Section */}
-      <Route path="partenaire" element={<Partenaire />}>
-        <Route path="morale" element={<Morale />}>
-          <Route path="client" element={<Client />} />
-          <Route path="fournisseur" element={<Fournisseur />} />
-        </Route>
-        <Route path="physique" element={<Physique />}>
-          <Route path="chauffeur" element={<Chauffeur />} />
-          <Route path="client" element={<Client />} />
-          <Route path="fournisseur" element={<Fournisseur />} />
-        </Route>
-      </Route>
-
-      {/* Camion Section */}
-      <Route path="camion" element={<Camion />}>
-        <Route path="cabine" element={<Cabine />} />
-        <Route path="remorque" element={<Remorque />} />
-      </Route>
-    </Route>
-  )
-);
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/route";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </React.StrictMode>
 );
---
+++ src/routes/route.jsx
@@ -0,0 +1,44 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Partenaire from "../pages/partenaire/Partenaire";
+import Morale from "../pages/partenaire/Morale";
+import Physique from "../pages/partenaire/Physique";
+import Client from "../pages/partenaire/Client";
+import Fournisseur from "../pages/partenaire/Fournisseur";
+import Chauffeur from "../pages/partenaire/Chauffeur";
+import Camion from "../pages/camion/Camion";
+import Cabine from "../pages/camion/Cabine";
+import Remorque from "../pages/camion/Remorque";
+import Layout from "../components/Layout"; // Un layout avec <Outlet />
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+
+        {/* Partenaire Section */}
+        <Route path="partenaire" element={<Partenaire />}>
+          <Route path="morale" element={<Morale />}>
+            <Route path="client" element={<Client />} />
+            <Route path="fournisseur" element={<Fournisseur />} />
+          </Route>
+          <Route path="physique" element={<Physique />}>
+            <Route path="chauffeur" element={<Chauffeur />} />
+            <Route path="client" element={<Client />} />
+            <Route path="fournisseur" element={<Fournisseur />} />
+          </Route>
+        </Route>
+
+        {/* Camion Section */}
+        <Route path="camion" element={<Camion />}>
+          <Route path="cabine" element={<Cabine />} />
+          <Route path="remorque" element={<Remorque />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default AppRoutes;
