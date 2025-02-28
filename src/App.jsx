fa8e1fd900c2f096a659bc1629e7adfe29b75034
ahmedAbghainouz <ahmed.abghnz@gmail.com>
2025-02-28 05:19:55 +0000

    only the component changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import { RouterProvider } from 'react-router-dom';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import router from './main';  // Ensure you import your router setup
-import './App.css';
+import React from "react";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Partenaire from "./pages/Partenaire";
+import Morale from "./pages/Morale";
+import Physique from "./pages/Physique";
+import Client from "./pages/Client";
+import Fournisseur from "./pages/Fournisseur";
+import Chauffeur from "./pages/Chauffeur";
+import Camion from "./pages/Camion";
+import Cabine from "./pages/Cabine";
+import Remorque from "./pages/Remorque";
+import Layout from "./components/Layout";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+// Define Routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="partenaire" element={<Partenaire />}>
+        <Route path="morale" element={<Morale />}>
+          <Route path="client" element={<Client />} />
+          <Route path="fournisseur" element={<Fournisseur />} />
+        </Route>
+        <Route path="physique" element={<Physique />}>
+          <Route path="chauffeur" element={<Chauffeur />} />
+          <Route path="client" element={<Client />} />
+          <Route path="fournisseur" element={<Fournisseur />} />
+        </Route>
+      </Route>
+      <Route path="camion" element={<Camion />}>
+        <Route path="cabine" element={<Cabine />} />
+        <Route path="remorque" element={<Remorque />} />
+      </Route>
+    </Route>
+  )
 );
+
+export default router;
