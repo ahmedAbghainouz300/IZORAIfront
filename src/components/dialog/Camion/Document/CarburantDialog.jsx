b3d61772d43cd8c545a238f844cffcb272435095
ahmedAbghainouz <ahmed.abghnz@gmail.com>
2025-03-12 03:25:58 +0000

    documents management

--- src/components/dialog/Camion/Document/CarburantDialog.jsx
+++ src/components/dialog/Camion/Document/CarburantDialog.jsx
@@ -7,6 +7,9 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker"; // Use MobileDatePicker
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 
 export default function CarburantDialog({ vopen, onClose }) {
   const [carburantData, setCarburantData] = React.useState({
@@ -21,61 +24,65 @@ export default function CarburantDialog({ vopen, onClose }) {
     const { name, value } = e.target;
     setCarburantData({ ...carburantData, [name]: value });
   };
-
+  const handleDateChange = (name) => (newValue) => {
+    setCarburantData({ ...carburantData, [name]: newValue });
+  };
   const handleSubmit = () => {
     console.log("Carburant Data:", carburantData);
     onClose();
   };
 
   return (
-    <Dialog open={vopen} onClose={onClose}>
-      <DialogTitle>Ajouter Carburant</DialogTitle>
-      <DialogContent>
-        <TextField
-          fullWidth
-          label="Date de Remplissage"
-          name="dateRemplissage"
-          value={carburantData.dateRemplissage}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Quantité (Litres)"
-          name="quantiteLitres"
-          value={carburantData.quantiteLitres}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Prix par Litre"
-          name="prixParLitre"
-          value={carburantData.prixParLitre}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Kilométrage Actuel"
-          name="kilometrageActuel"
-          value={carburantData.kilometrageActuel}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Type de Carburant"
-          name="typeCarburant"
-          value={carburantData.typeCarburant}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Annuler</Button>
-        <Button onClick={handleSubmit}>Enregistrer</Button>
-      </DialogActions>
-    </Dialog>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Dialog open={vopen} onClose={onClose}>
+        <DialogTitle>Ajouter Carburant</DialogTitle>
+        <DialogContent>
+          <MobileDatePicker
+            label="Date de Remplissage"
+            value={carburantData.dateRemplissage}
+            onChange={handleDateChange("dateRemplissage")}
+            renderInput={(params) => (
+              <TextField {...params} fullWidth margin="normal" />
+            )}
+          />
+          <TextField
+            fullWidth
+            label="Quantité (Litres)"
+            name="quantiteLitres"
+            value={carburantData.quantiteLitres}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Prix par Litre"
+            name="prixParLitre"
+            value={carburantData.prixParLitre}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Kilométrage Actuel"
+            name="kilometrageActuel"
+            value={carburantData.kilometrageActuel}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Type de Carburant"
+            name="typeCarburant"
+            value={carburantData.typeCarburant}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Annuler</Button>
+          <Button onClick={handleSubmit}>Enregistrer</Button>
+        </DialogActions>
+      </Dialog>
+    </LocalizationProvider>
   );
 }
--- src/components/dialog/Camion/Document/EntretienDialog.jsx
+++ src/components/dialog/Camion/Document/EntretienDialog.jsx
@@ -11,7 +11,9 @@ import {
   InputLabel,
   FormControl,
 } from "@mui/material";
-
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker"; // Use MobileDatePicker
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 export default function EntretienDialog({ vopen, onClose }) {
   const [entretienData, setEntretienData] = React.useState({
     dateEntretien: "",
@@ -31,83 +33,89 @@ export default function EntretienDialog({ vopen, onClose }) {
   const handleCamionChange = (event) => {
     setEntretienData({ ...entretienData, camion: event.target.value });
   };
-
+  const handleDateChange = (name) => (newValue) => {
+    setEntretienData({ ...entretienData, [name]: newValue });
+  };
   const handleSubmit = () => {
     console.log("Entretien Data:", entretienData);
     onClose();
   };
 
   return (
-    <Dialog open={vopen} onClose={onClose}>
-      <DialogTitle>Ajouter Entretien</DialogTitle>
-      <DialogContent>
-        <TextField
-          fullWidth
-          label="Date d'Entretien"
-          name="dateEntretien"
-          value={entretienData.dateEntretien}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Type d'Entretien"
-          name="typeEntretien"
-          value={entretienData.typeEntretien}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Description"
-          name="description"
-          value={entretienData.description}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Coût"
-          name="cout"
-          value={entretienData.cout}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Prochain Entretien"
-          name="prochainEntretien"
-          value={entretienData.prochainEntretien}
-          onChange={handleInputChange}
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          label="Statut"
-          name="statut"
-          value={entretienData.statut}
-          onChange={handleInputChange}
-          margin="normal"
-        />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Dialog open={vopen} onClose={onClose}>
+        <DialogTitle>Ajouter Entretien</DialogTitle>
+        <DialogContent>
+          <MobileDatePicker
+            label="Date d'Entretien"
+            value={entretienData.dateEntretien}
+            onChange={handleDateChange("dateEntretien")}
+            renderInput={(params) => (
+              <TextField {...params} fullWidth margin="normal" />
+            )}
+          />
 
-        {/* New Camion Selection Field */}
-        <FormControl fullWidth margin="normal">
-          <InputLabel>Camion</InputLabel>
-          <Select
-            value={entretienData.camion}
-            onChange={handleCamionChange}
-            name="camion"
-          >
-            <MenuItem value="Camion 1">Camion 1</MenuItem>
-            <MenuItem value="Camion 2">Camion 2</MenuItem>
-            <MenuItem value="Camion 3">Camion 3</MenuItem>
-          </Select>
-        </FormControl>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Annuler</Button>
-        <Button onClick={handleSubmit}>Enregistrer</Button>
-      </DialogActions>
-    </Dialog>
+          <TextField
+            fullWidth
+            label="Type d'Entretien"
+            name="typeEntretien"
+            value={entretienData.typeEntretien}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Description"
+            name="description"
+            value={entretienData.description}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Coût"
+            name="cout"
+            value={entretienData.cout}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <MobileDatePicker
+            label="Prochain Entretien"
+            value={entretienData.prochainEntretien}
+            onChange={handleDateChange("prochainEntretien")}
+            renderInput={(params) => (
+              <TextField {...params} fullWidth margin="normal" />
+            )}
+          />
+
+          <TextField
+            fullWidth
+            label="Statut"
+            name="statut"
+            value={entretienData.statut}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+
+          {/* New Camion Selection Field */}
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Camion</InputLabel>
+            <Select
+              value={entretienData.camion}
+              onChange={handleCamionChange}
+              name="camion"
+            >
+              <MenuItem value="Camion 1">Camion 1</MenuItem>
+              <MenuItem value="Camion 2">Camion 2</MenuItem>
+              <MenuItem value="Camion 3">Camion 3</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Annuler</Button>
+          <Button onClick={handleSubmit}>Enregistrer</Button>
+        </DialogActions>
+      </Dialog>
+    </LocalizationProvider>
   );
 }
