da38a3cbe9ebc160d7dc100a83abc3716576a530
AnasLaHBouB <anaslahbou11@gmail.com>
2025-04-18 10:17:17 +0100

    fixer le vooyage par ahmed

--- src/service/axiosClient.jsx
+++ src/service/axiosClient.jsx
@@ -4,6 +4,7 @@ const axiosClient = axios.create({
   baseURL: "http://localhost:8080/",
   headers: {
     "Content-Type": "application/json"
+    // "Authorization-":
     // "bearer": localStorage.getItem("token"),
   },
 });
